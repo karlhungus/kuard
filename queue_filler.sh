48c801bb88464c0e2cd90866cb2cbbcaef233274
Izaak Alpert (karlhungus) <ialpert@gmail.com>
2017-10-30 20:17:32 -0400

    fix a typo

--- queue_filler.sh
+++ queue_filler.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-# to use this
+# #to use this
 # kubectl apply -f rs-queue.yml
 #
-# in a different terminal:
+# #in a different terminal:
 #
 # QUEUE_POD=$(kubectl get pods -l app=work-queue,compoennt=queue -o jsonpath='{.items[0].metadata.name}')
 # kubectl port-forward $QUEUE_POD 8080:8080
 #
 #
 # kubectl apply -f service-queue.yml
+# 
+# #run this script, finally run:
+# kubectl apply -f job-consumer.yml
 
 # create a work queue called 'keygen'
 curl -X PUT localhost:8080/memq/server/queues/keygen
